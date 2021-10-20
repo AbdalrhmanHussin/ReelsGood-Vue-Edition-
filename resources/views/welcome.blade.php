349a0ad97f6aec90abdaf7009f80c39b92477842
abdalrhman hussin <abdalrhmanhussin44@gmail.com>
2021-10-20 04:39:18 +0200

    navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Reelsgood</title>
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
         <!-- Fonts -->
     </head>
     <body class="antialiased">
     <div id="app">
         <app-component></app-component>
     </div>  
-    <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
